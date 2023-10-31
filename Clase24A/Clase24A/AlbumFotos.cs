4ae27f0e54f6b0d8f1f11c59b6a4fb28c25ea030
Iri <89229099+IAgretti@users.noreply.github.com>
2023-10-31 10:16:06 -0300

    Constructores
    Clase AlbumFotos

---
+++ Clase24A/Clase24A/AlbumFotos.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Clase24A
+{
+	
+	public class AlbumFotos
+	{
+//		private int numPag;
+//		public int NumPag {
+//			get {return numPag;}
+//			set {numPag = value;}
+//		}
+		public int NumPag {get; set;}
+		
+		public AlbumFotos()
+		{
+			NumPag = 16;
+		}
+		public AlbumFotos(int p){
+			NumPag = p;
+		}
+	}
+	class SuperAlbum: AlbumFotos {
+		public SuperAlbum(){
+			NumPag = 64;
+		}
+	}
+}
---
+++ Clase24A/Clase24A/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Clase24A
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			AlbumFotos af = new AlbumFotos();
+			AlbumFotos af2 = new AlbumFotos(24);
+			SuperAlbum sa = new SuperAlbum();
+			Console.WriteLine("El álbum predeterminado tiene " + af.NumPag + " páginas.");
+			Console.WriteLine("El segundo álbum tiene " + af2.NumPag + " páginas.");
+			Console.WriteLine("El súper álbum tiene " + sa.NumPag + " páginas.\n");
+			      
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
